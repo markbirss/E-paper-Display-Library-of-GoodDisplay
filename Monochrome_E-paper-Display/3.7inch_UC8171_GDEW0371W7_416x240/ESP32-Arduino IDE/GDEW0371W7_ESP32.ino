9e148b5b113d77bd32f270abb0749323deb181fc
Good Display <57305534+gooddisplayepaper@users.noreply.github.com>
2022-11-07 17:12:07 +0800

    Add files via upload

---
+++ Monochrome_E-paper-Display/3.7inch_UC8171_GDEW0371W7_416x240/ESP32-Arduino IDE/GDEW0371W7_ESP32.ino
@@ -0,0 +1,207 @@
+#include <SPI.h>
+#include"Ap_29demo.h"
+//IO settings
+int BUSY_Pin = A14; 
+int RES_Pin = A15; 
+int DC_Pin = A16; 
+int CS_Pin = A17; 
+//SCLK--GPIO23
+//MOSI---GPIO18
+
+
+#define EPD_W21_CS_0 digitalWrite(CS_Pin,LOW)
+#define EPD_W21_CS_1 digitalWrite(CS_Pin,HIGH)
+#define EPD_W21_DC_0  digitalWrite(DC_Pin,LOW)
+#define EPD_W21_DC_1  digitalWrite(DC_Pin,HIGH)
+#define EPD_W21_RST_0 digitalWrite(RES_Pin,LOW)
+#define EPD_W21_RST_1 digitalWrite(RES_Pin,HIGH)
+#define isEPD_W21_BUSY digitalRead(BUSY_Pin)
+////////FUNCTION//////
+void SPI_Write(unsigned char value);
+void EPD_W21_WriteDATA(unsigned char command);
+void EPD_W21_WriteCMD(unsigned char command);
+//EPD
+void EPD_W21_Init(void);
+void EPD_init(void);
+void PIC_display1(void);
+void PIC_display2(void);
+void EPD_sleep(void);
+void EPD_refresh(void);
+void lcd_chkstatus(void);
+void PIC_display_Clean(void);
+unsigned char HRES,VRES_byte1,VRES_byte2;
+
+void setup() {
+   pinMode(BUSY_Pin, INPUT); 
+   pinMode(RES_Pin, OUTPUT);  
+   pinMode(DC_Pin, OUTPUT);    
+   pinMode(CS_Pin, OUTPUT);    
+   //SPI
+   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0)); 
+   SPI.begin ();  
+}
+//Tips//
+/*When the electronic paper is refreshed in full screen, the picture flicker is a normal phenomenon, and the main function is to clear the display afterimage in the previous picture.
+  When the local refresh is performed, the screen does not flash.*/
+/*When you need to transplant the driver, you only need to change the corresponding IO. The BUSY pin is the input mode and the others are the output mode. */
+
+void loop() {
+  while(1)
+  {
+    //PICTURE1
+    EPD_init(); //EPD init
+    PIC_display1();
+    EPD_refresh();//EPD_refresh   
+    EPD_sleep();//EPD_sleep,Sleep instruction is necessary, please do not delete!!!
+    delay(3000);
+    
+    //PICTURE2
+    EPD_init(); //EPD init
+    PIC_display2();
+    EPD_refresh();//EPD_refresh   
+    EPD_sleep();//EPD_sleep,Sleep instruction is necessary, please do not delete!!!
+    delay(3000);
+    
+    //PICTURE Clean
+    EPD_init(); //EPD init
+    PIC_display_Clean();
+    EPD_refresh();//EPD_refresh   
+    EPD_sleep();//EPD_sleep,Sleep instruction is necessary, please do not delete!!!
+    while(1);
+
+  }
+
+
+}
+
+
+
+///////////////////EXTERNAL FUNCTION////////////////////////////////////////////////////////////////////////
+void SPI_Write(unsigned char value)                                    
+{                                                           
+    SPI.transfer(value);
+
+}
+
+void EPD_W21_WriteCMD(unsigned char command)
+{
+  EPD_W21_CS_0;                   
+  EPD_W21_DC_0;   // command write
+  SPI_Write(command);
+  EPD_W21_CS_1;
+}
+void EPD_W21_WriteDATA(unsigned char command)
+{
+  EPD_W21_CS_0;                   
+  EPD_W21_DC_1;   // command write
+  SPI_Write(command);
+  EPD_W21_CS_1;
+}
+
+
+
+/////////////////EPD settings Functions/////////////////////
+void EPD_W21_Init(void)
+{
+  EPD_W21_RST_0;    // Module reset
+  delay(10); //At least 10ms
+  EPD_W21_RST_1;
+  delay(10);  
+}
+void EPD_init(void)
+{
+    HRES=0xf0;            //240
+    VRES_byte1=0x01;        //416
+    VRES_byte2=0xa0;
+  
+    EPD_W21_Init(); //Electronic paper IC reset
+    delay(100);  
+    EPD_W21_WriteCMD(0x06);         //boost soft start
+    EPD_W21_WriteDATA (0x17);   //A
+    EPD_W21_WriteDATA (0x17);   //B
+    EPD_W21_WriteDATA (0x1d);   //C       
+
+    EPD_W21_WriteCMD(0x04);  //Power on
+    lcd_chkstatus();        //waiting for the electronic paper IC to release the idle signal
+
+    EPD_W21_WriteCMD(0x00);     //panel setting
+    EPD_W21_WriteDATA(0x1f);    //LUT from OTP--240x416
+
+    EPD_W21_WriteCMD(0x61);     //resolution setting
+    EPD_W21_WriteDATA (HRES);          
+    EPD_W21_WriteDATA (VRES_byte1);   
+    EPD_W21_WriteDATA (VRES_byte2);
+
+    EPD_W21_WriteCMD(0x50);         
+    EPD_W21_WriteDATA (0x23);   
+    EPD_W21_WriteDATA (0x07); 
+}
+void EPD_refresh(void)
+{
+    EPD_W21_WriteCMD(0x12);     //DISPLAY REFRESH   
+    delay(100);          //!!!The delay here is necessary, 200uS at least!!!     
+    lcd_chkstatus();
+} 
+void EPD_sleep(void)
+{
+    EPD_W21_WriteCMD(0X50);  //VCOM AND DATA INTERVAL SETTING  
+    EPD_W21_WriteDATA(0xf7);
+       
+    EPD_W21_WriteCMD(0X02);   //power off
+    lcd_chkstatus();
+    EPD_W21_WriteCMD(0X07);   //deep sleep
+    EPD_W21_WriteDATA(0xA5);
+}
+
+
+void PIC_display1(void)
+{
+    unsigned int i;
+    EPD_W21_WriteCMD(0x10);        //Transfer old data
+    for(i=0;i<12480;i++)      
+    EPD_W21_WriteDATA(0xff); 
+  
+    EPD_W21_WriteCMD(0x13);        //Transfer new data
+    for(i=0;i<12480;i++)      
+     EPD_W21_WriteDATA(pgm_read_byte(&gImage_1[i]));
+
+}
+void PIC_display2(void)
+{
+    unsigned int i;
+    EPD_W21_WriteCMD(0x10);        //Transfer old data
+    for(i=0;i<12480;i++)      
+    EPD_W21_WriteDATA(0xff); 
+  
+    EPD_W21_WriteCMD(0x13);        //Transfer new data
+    for(i=0;i<12480;i++)      
+     EPD_W21_WriteDATA(pgm_read_byte(&gImage_2[i]));
+
+}
+void PIC_display_Clean(void)
+{
+    unsigned int i;
+    EPD_W21_WriteCMD(0x10);        //Transfer old data
+    for(i=0;i<12480;i++)       
+  {
+    EPD_W21_WriteDATA(0xff);
+  }
+  
+    EPD_W21_WriteCMD(0x13);        //Transfer new data
+    for(i=0;i<12480;i++)       
+  {
+    EPD_W21_WriteDATA(0xff);
+  }
+}
+void lcd_chkstatus(void)
+{
+  unsigned char busy;
+  do
+  {
+    EPD_W21_WriteCMD(0x71);
+    busy = isEPD_W21_BUSY;
+    busy =!(busy & 0x01);        
+  }
+  while(busy);   
+  delay(200);                       
+}
