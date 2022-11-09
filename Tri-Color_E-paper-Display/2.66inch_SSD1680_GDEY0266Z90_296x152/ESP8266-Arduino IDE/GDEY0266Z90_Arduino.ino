3afe688f816bfdcffe00856518f9cd85c0739ca4
Good Display <57305534+gooddisplayepaper@users.noreply.github.com>
2022-11-09 11:45:31 +0800

    Add files via upload

---
+++ Tri-Color_E-paper-Display/2.66inch_SSD1680_GDEY0266Z90_296x152/ESP8266-Arduino IDE/GDEY0266Z90_Arduino.ino
@@ -0,0 +1,234 @@
+#include <SPI.h>
+#include"Ap_29demo.h"
+//IO settings
+int BUSY_Pin = D0; 
+int RES_Pin = D1; 
+int DC_Pin = D2; 
+int CS_Pin = D4; 
+//HSCLK---D5
+//HMOSI--D7
+#define EPD_W21_CS_0 digitalWrite(CS_Pin,LOW)
+#define EPD_W21_CS_1 digitalWrite(CS_Pin,HIGH)
+#define EPD_W21_DC_0  digitalWrite(DC_Pin,LOW)
+#define EPD_W21_DC_1  digitalWrite(DC_Pin,HIGH)
+#define EPD_W21_RST_0 digitalWrite(RES_Pin,LOW)
+#define EPD_W21_RST_1 digitalWrite(RES_Pin,HIGH)
+#define isEPD_W21_BUSY digitalRead(BUSY_Pin)
+//296*152///////////////////////////////////////
+
+#define MONOMSB_MODE 1
+#define MONOLSB_MODE 2 
+#define RED_MODE     3
+
+#define MAX_LINE_BYTES 19  //=152/8
+#define MAX_COLUMN_BYTES  296
+
+#define ALLSCREEN_GRAGHBYTES  5624
+
+////////FUNCTION//////   
+void SPI_Delay(unsigned char xrate);
+void SPI_Write(unsigned char value);
+void Epaper_Write_Command(unsigned char command);
+void Epaper_Write_Data(unsigned char command);
+//EPD
+void Epaper_READBUSY(void);
+void Epaper_Spi_WriteByte(unsigned char TxData);
+void Epaper_Write_Command(unsigned char cmd);
+void Epaper_Write_Data(unsigned char data);
+
+void EPD_HW_Init(void); //Electronic paper initialization
+void EPD_Update(void);
+void EPD_DeepSleep(void);
+//Display 
+void EPD_WhiteScreen_ALL(const unsigned char *BW_datas,const unsigned char *R_datas);
+void EPD_WhiteScreen_ALL_Clean(void);
+
+void Sys_run(void)
+{
+   ESP.wdtFeed(); //Feed dog to prevent system reset
+  }
+void LED_run(void)
+{
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+  delay(500);
+  }
+void setup() {
+   pinMode(BUSY_Pin, INPUT); 
+   pinMode(RES_Pin, OUTPUT);  
+   pinMode(DC_Pin, OUTPUT);    
+   pinMode(CS_Pin, OUTPUT);    
+   //SPI
+   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0)); 
+   SPI.begin ();  
+}
+
+//Tips//
+/*When the electronic paper is refreshed in full screen, the picture flicker is a normal phenomenon, and the main function is to clear the display afterimage in the previous picture.
+  When the local refresh is performed, the screen does not flash.*/
+/*When you need to transplant the driver, you only need to change the corresponding IO. The BUSY pin is the input mode and the others are the output mode. */
+  
+void loop() {
+  while(1)
+  { 
+    //Full screen refresh
+    EPD_HW_Init(); //Electronic paper initialization
+    EPD_WhiteScreen_ALL(gImage_BW1,gImage_R1); //Refresh the picture in full screen   
+    EPD_DeepSleep(); //Enter deep sleep,Sleep instruction is necessary, please do not delete!!! 
+    delay(3000);   
+    
+    //Clean
+    EPD_HW_Init(); //Electronic paper initialization
+    EPD_WhiteScreen_ALL_Clean();
+    EPD_DeepSleep(); //Enter deep sleep,Sleep instruction is necessary, please do not delete!!!
+  while(1) 
+    {
+     Sys_run();//System run
+     LED_run();//Breathing lamp
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+///////////////////EXTERNAL FUNCTION////////////////////////////////////////////////////////////////////////
+
+void SPI_Write(unsigned char value)                                    
+{                                                           
+    SPI.transfer(value);
+
+}
+
+void Epaper_Write_Command(unsigned char command)
+{
+  EPD_W21_CS_0;                   
+  EPD_W21_DC_0;   // command write
+  SPI_Write(command);
+  EPD_W21_CS_1;
+}
+void Epaper_Write_Data(unsigned char command)
+{
+  EPD_W21_CS_0;                   
+  EPD_W21_DC_1;   // command write
+  SPI_Write(command);
+  EPD_W21_CS_1;
+}
+
+/////////////////EPD settings Functions/////////////////////
+
+void EPD_HW_Init(void)
+{
+  EPD_W21_RST_0;  // Module reset      
+  delay(10); //At least 10ms delay 
+  EPD_W21_RST_1; 
+  delay(10); //At least 10ms delay   
+  
+  Epaper_READBUSY();   
+  Epaper_Write_Command(0x12);  //SWRESET
+  Epaper_READBUSY();   
+    
+  Epaper_Write_Command(0x01); //Driver output control      
+  Epaper_Write_Data(0x27);
+  Epaper_Write_Data(0x01);
+  Epaper_Write_Data(0x00);
+
+  Epaper_Write_Command(0x11); //data entry mode       
+  Epaper_Write_Data(0x01);
+
+  Epaper_Write_Command(0x44); //set Ram-X address start/end position   
+  Epaper_Write_Data(0x00);
+  Epaper_Write_Data(0x12);    //0x12-->(15+1)*8=152
+
+  Epaper_Write_Command(0x45); //set Ram-Y address start/end position          
+  Epaper_Write_Data(0x27);   //0x0127-->(295+1)=296
+  Epaper_Write_Data(0x01);
+  Epaper_Write_Data(0x00);
+  Epaper_Write_Data(0x00); 
+
+  Epaper_Write_Command(0x3C); //BorderWavefrom
+  Epaper_Write_Data(0x05);  
+      
+  Epaper_Write_Command(0x18); //Read built-in temperature sensor
+  Epaper_Write_Data(0x80);  
+  
+  Epaper_Write_Command(0x21); //  Display update control
+  Epaper_Write_Data(0x00);  
+  Epaper_Write_Data(0x80);  
+
+  Epaper_Write_Command(0x4E);   // set RAM x address count to 0;
+  Epaper_Write_Data(0x00);
+  Epaper_Write_Command(0x4F);   // set RAM y address count to 0X199;    
+  Epaper_Write_Data(0x27);
+  Epaper_Write_Data(0x01);
+  Epaper_READBUSY();
+}
+//////////////////////////////All screen update////////////////////////////////////////////
+void EPD_WhiteScreen_ALL(const unsigned char *BW_datas,const unsigned char *R_datas)
+{
+   unsigned int i;  
+  Epaper_Write_Command(0x24);   //write RAM for black(0)/white (1)
+   for(i=0;i<ALLSCREEN_GRAGHBYTES;i++)
+   {               
+     Epaper_Write_Data(pgm_read_byte(&BW_datas[i]));
+   }
+  Epaper_Write_Command(0x26);   //write RAM for black(0)/white (1)
+   for(i=0;i<ALLSCREEN_GRAGHBYTES;i++)
+   {               
+     Epaper_Write_Data(~pgm_read_byte(&R_datas[i]));
+   }
+   EPD_Update();   
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+void EPD_Update(void)
+{   
+  Epaper_Write_Command(0x22); //Display Update Control
+  Epaper_Write_Data(0xF7);   
+  Epaper_Write_Command(0x20);  //Activate Display Update Sequence
+  Epaper_READBUSY();   
+
+}
+
+void EPD_DeepSleep(void)
+{  
+  Epaper_Write_Command(0x10); //enter deep sleep
+  Epaper_Write_Data(0x01); 
+  delay(100);
+}
+void Epaper_READBUSY(void)
+{ 
+  while(1)
+  {   //=1 BUSY
+     if(isEPD_W21_BUSY==0) break;
+     ESP.wdtFeed(); //Feed dog to prevent system reset
+  }  
+}
+
+void EPD_WhiteScreen_ALL_Clean(void)
+{
+   unsigned int i;  
+  Epaper_Write_Command(0x24);   //write RAM for black(0)/white (1)
+   for(i=0;i<ALLSCREEN_GRAGHBYTES;i++)
+   {               
+     Epaper_Write_Data(0xff);
+   }
+  Epaper_Write_Command(0x26);   //write RAM for black(0)/white (1)
+   for(i=0;i<ALLSCREEN_GRAGHBYTES;i++)
+   {               
+     Epaper_Write_Data(0x00);
+   }
+   EPD_Update();   
+}
+
+
+
+//////////////////////////////////END//////////////////////////////////////////////////
